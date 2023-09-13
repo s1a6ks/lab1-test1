8f7c55fe1867e63ad0971ab541746adbf87f504e
Макс Горецький <s1a6ks@gmail.com>
2023-09-13 08:25:12 +0300

    C:/Program Files/Git/

---
+++ test1/test1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int X = 5;
+    int Y = 7;
+    int Z = 10;
+   
+    int suma = X + Y + Z;
+
+   
+    int multiplication = X * Y * Z;
+
+   
+    double geometric = cbrt(fabs(X) * fabs(Y) * fabs(Z));
+
+   
+    printf("avg sum: %d\n", suma);
+    printf("avg multiplication %d\n", multiplication);
+    printf("avg geometric: %lf\n", geometric);
+
+    return 0;
+}
